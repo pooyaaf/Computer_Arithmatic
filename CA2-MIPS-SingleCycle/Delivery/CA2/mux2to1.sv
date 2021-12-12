66157a9dd6a8b4b5bb187022807ad736bf5a9011
ryhnAp <69680559+ryhnAp@users.noreply.github.com>
2021-12-12 17:23:15 +0330

    delete last correction

--- CA2-MIPS-SingleCycle/Delivery/CA2/mux2to1.sv
+++
@@ -1,8 +0,0 @@
-module mux2to1 #(parameter SIZE = 32) (i0, i1, sel, out);
-    input [SIZE-1:0] i0, i1;
-    input sel;
-    output [SIZE-1:0] out;
-    
-    assign out = (sel == 1'b1) ? i1 : i0;
-    
-endmodule
